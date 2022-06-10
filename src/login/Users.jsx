cafb0575e11541a1c6c76b015e7e4a70714402d9
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2022-06-10 20:52:56 -0300

    finish login card

--- src/login/Users.jsx
+++ src/login/Users.jsx
@@ -4,6 +4,7 @@ import { LoginButton } from './Login'
 import { LogoutButton } from './Logout'
 import { ProfileUser } from './Profile'
 import { useAuth0 } from "@auth0/auth0-react"
+import { Link } from 'react-router-dom'
 
 const Users = () => {
   const { isAuthenticated } = useAuth0()
@@ -15,7 +16,31 @@ const Users = () => {
           <ProfileUser />
           <LogoutButton />
         </>
-        : <LoginButton />
+        : 
+        <>
+        <div className="container-back">
+          <div className="container-card">
+            <h1>Inicia sesion</h1>
+              <h3>Si ustedes se loguea tendra muchos beneficios como por ejemplo:</h3>
+              <ul>
+                <li>
+                  - Podra crear sus rutinas personalizadas.
+                </li>
+                <li>
+                  - Tendra acceso a una dieta adaptada a su gusto.
+                </li>
+                <li>
+                  - Explicaciones de los ejercicios.
+                </li>
+              </ul>
+            <LoginButton />
+            <Link className='back-to' to={"/"}><i class="ri-arrow-left-line"></i>Volver</Link>
+          </div>
+          <div className="back-color">
+
+          </div>
+        </div>
+        </>
       }
     </div>
   )
